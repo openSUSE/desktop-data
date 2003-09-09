d205cbace3272f3182ba43505642da6a61432ba9
Adrian Schroeter <adrian@suse.de>
2003-09-09 11:58:53 +0000

    fix it

--- etc/profile.d/desktop-data.csh
+++ etc/profile.d/desktop-data.csh
@@ -1,5 +1,6 @@
 # set the default mouse cursor theme for X11
 if ( -e /etc/sysconfig/windowmanager ) then
+  source /etc/sysconfig/windowmanager
   setenv XCURSOR_THEME "$X_MOUSE_CURSOR"
 endif
 
--- etc/profile.d/desktop-data.sh
+++ etc/profile.d/desktop-data.sh
@@ -1,5 +1,6 @@
 # set the default mouse cursor theme for X11
-if [ -r /etc/sysconfig/windowmanager ] then
+if [ -r /etc/sysconfig/windowmanager ]; then
+  source /etc/sysconfig/windowmanager
   export XCURSOR_THEME="$X_MOUSE_CURSOR"
-endif
+fi
 
