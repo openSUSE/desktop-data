c75afda7622be6184aab1f99f4958736435beeab
Adrian Schroeter <adrian@suse.de>
2003-09-10 07:56:28 +0000

    make it like werner says

--- etc/profile.d/desktop-data.sh
+++ etc/profile.d/desktop-data.sh
@@ -1,6 +1,7 @@
 # set the default mouse cursor theme for X11
 if [ -r /etc/sysconfig/windowmanager ]; then
-  source /etc/sysconfig/windowmanager
-  export XCURSOR_THEME="$X_MOUSE_CURSOR"
+  . /etc/sysconfig/windowmanager
+  XCURSOR_THEME="$X_MOUSE_CURSOR"
+  export XCURSOR_THEME
 fi
 
