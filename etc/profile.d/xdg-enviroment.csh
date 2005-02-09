5178266b793f9d3e76613bc1a92c546565a7ef18
Adrian Schroeter <adrian@suse.de>
2005-02-09 13:12:19 +0000

    moved to aaa_base

--- etc/profile.d/xdg-enviroment.csh
+++
@@ -1,3 +0,0 @@
-if ( ${?XDG_DATA_DIRS} ) then
-    setenv XDG_DATA_DIRS /usr/share/:/usr/local/share/:/etc/opt/kde3/share/:/opt/kde3/share/:/opt/gnome/share/
-endif
--- etc/profile.d/xdg-enviroment.sh
+++
@@ -1,3 +0,0 @@
-if test -z "$XDG_DATA_DIRS" ; then
-    export XDG_DATA_DIRS=/usr/share/:/usr/local/share/:/etc/opt/kde3/share/:/opt/kde3/share/:/opt/gnome/share/
-fi
